440eba2d2fd62e25a92d1d87b76e0d425b5e7340
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-05-23 17:46:46 -0400

    working on saving zip files

--- client/scripts/index_src.js
+++ client/scripts/index_src.js
@@ -9,6 +9,10 @@ window.onload = function(){
 	manuscriptapp.init();
 };
 
+// manuscript.downloadRenderedOutput = function(obj){
+// 	console.log("overwrite method",obj.config());
+// };
+
 manuscriptapp.on("intializedManuscript",function(data){
 	console.log("loaded manuscript",data);
 });
